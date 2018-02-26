077a60658db626929fe87e6341a1505e67ac5d89
T-tssxuan <2270360114@qq.com>
2018-02-26 22:38:37 +0800

    [alter]

---
+++ code/usefully/log.py
@@ -0,0 +1,27 @@
+import os 
+import logging
+import sys
+
+class Tools:
+    log_file = ''
+
+    def set_log_file(file_name):
+        Tools.log_file = file_name
+
+    def get_logger(name='defualt', level=logging.INFO):
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+
+        fmt = '%(asctime)s %(levelname)-8s [' + name + '] %(message)s'
+        datefmt = '%Y-%m-%d %H:%M:%S'
+        formatter = logging.Formatter(fmt=fmt, datefmt=datefmt)
+
+        if Tools.log_file != '':
+            handler = logging.FileHandler('log.txt', mode='a')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
+        screen_handler = logging.StreamHandler(stream=sys.stdout)
+        screen_handler.setFormatter(formatter)
+        logger.addHandler(screen_handler)
+        return logger
